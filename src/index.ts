6d01c11af55e2feb621decffdcf3e7f07a2100c1
sidmohanty11 <sidmohanty11@gmail.com>
2021-08-31 18:47:36 +0530

    implementation of collectionViews

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
-import { User } from "./models/User";
+import { User, UserProps } from "./models/User";
 import { UserEdit } from "./views/UserEdit";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+  return User.create(json);
+});
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    const userList = new UserList(root, users);
+    userList.render();
+  } else {
+    throw new Error('root element not found');
+  }
+});
+
+users.fetch();
 
 const user = User.create({ id: 1 });
 user.fetch();
-
-const root = document.getElementById('root');
-if (root) {
-  const userEdit = new UserEdit(root, user);
+const root2 = document.getElementById('root2');
+if (root2) {
+  const userEdit = new UserEdit(root2, user);
   userEdit.render();
 } else {
   throw new Error('root element not found');
 }
+
 
